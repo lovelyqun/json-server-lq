0f17d4efeb99b5460de8982788aab12d1a786eb2
lovelyqun <729887058@qq.com>
2024-06-12 17:59:17 +0800

    feat:修改端口

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-const port = process.env.PORT || 9529; // Vercel 会提供 PORT 环境变量
+const port = process.env.PORT || 3000; // Vercel 会提供 PORT 环境变量
 
 server.use(middlewares)
 server.use(router)
