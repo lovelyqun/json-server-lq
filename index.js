524827a86dca546ce6a0b1e524ddda862dcb6353
lovelyqun <729887058@qq.com>
2024-06-13 09:11:19 +0800

    feat:修改文件

---
+++ index.js
@@ -0,0 +1,11 @@
+var jsonServer = require("json-server");
+var db = require("./db.js");
+var server = jsonServer.create();
+var router = jsonServer.router(db());
+var middlewares = jsonServer.defaults();
+var port = process.env.PORT || 5000;
+server.use(middlewares);
+server.use(router);
+server.listen(port, function () {
+  console.log("JSON Server is running on http://localhost:" + port);
+});
